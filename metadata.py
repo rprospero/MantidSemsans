e928c3f73ecc6c9448ab56f89a30b123a79009ce
Adam Washington <adam.washington@stfc.ac.uk>
2017-08-04 14:11:58 +0100

    Store run info in Mantid Table

--- metadata.py
+++ metadata.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 import re
 import datetime
-import json
 import os.path
 import xml.etree
+from mantid.simpleapi import CreateEmptyTableWorkspace, RenameWorkspace
 from .runtypes import HeData, RunData, QuickData
+
 
 def numbered_run(x):
     return re.match(r'(.+) run: (\d+)_SANS', x) or \
         re.match(r'MT Beam_SANS', x) or \
         re.match(r'D2. 2mm_SANS', x)
 
+
 def trans_run(x):
     return re.match(r".+_TRANS", x)
+
 
 def can_sans(x):
     return re.match(r"D2. 2mm_SANS", x)
 
+
 def can_trans(x):
     return re.match(r"D2. 2mm_TRANS", x)
+
 
 def direct_trans(x):
     return re.match(r"MT Beam_TRANS", x)
@@ -38,7 +43,7 @@ def convert_he(i):
 
 def load_helium_file(f):
     with open(f, "r") as infile:
-        header = infile.readline()
+        infile.readline()  # read header
         return [convert_he(line.split("\t"))
                 for line in infile]
 
@@ -79,11 +84,13 @@ JPATH = r'\\isis\inst$\NDXLARMOR\Instrument\logs\journal'
 
 
 def get_relevant_log(run):
-    with open(JPATH+"\journal_main.xml", "r") as infile:
+    with open(JPATH+r"\journal_main.xml", "r") as infile:
         journals = xml.etree.ElementTree.parse(infile)
     for child in journals.getroot():
-        if int(child.attrib["first_run"]) <= run <= int(child.attrib["last_run"]):
+        if int(child.attrib["first_run"]) <= run and \
+           run <= int(child.attrib["last_run"]):
             return child.attrib["name"]
+
 
 def get_xml_run_number(node):
     for child in node:
@@ -101,7 +108,7 @@ def get_log(runs):
     results = []
     with open(log_file, "r") as infile:
         journal = xml.etree.cElementTree.iterparse(infile)
-        for event, child in journal:
+        for _, child in journal:
             if "NXentry" in child.tag:
                 num = get_xml_run_number(child)
                 if num in runs:
@@ -113,7 +120,8 @@ def get_log(runs):
                                 param.text,
                                 "%Y-%m-%dT%H:%M:%S")
                         elif "duration" in param.tag:
-                            duration = datetime.timedelta(seconds=int(param.text))
+                            duration = datetime.timedelta(
+                                seconds=int(param.text))
                         elif "proton_charge" in param.tag:
                             proton_charge = float(param.text)
                     results.append(
@@ -140,8 +148,19 @@ def get_log(runs):
         else:
             d[run.sample] = [run]
 
-    with open(
-              os.path.join(
-                  r"C:\Users\auv61894\Dropbox\Science\Edler\Edler_May_2017",
-                  "example.json"), "w") as outfile:
-        json.dump(d, outfile)
+    for k, v in d.items():
+        my_table = CreateEmptyTableWorkspace()
+        my_table.addColumn("int", "Run Number")
+        my_table.addColumn("str", "Sample")
+        my_table.addColumn("float", "Scale")
+        my_table.addColumn("float", "He3 Start")
+        my_table.addColumn("float", "He3 End")
+        my_table.addColumn("int", "Trans run")
+        my_table.addColumn("int", "Can Sans run")
+        my_table.addColumn("int", "Can Trans run")
+        my_table.addColumn("int", "Direct Trans run")
+        my_table.addColumn("str", "Start time")
+
+        for run in v:
+            my_table.addRow(run)
+        RenameWorkspace(my_table, k+"_runs")
