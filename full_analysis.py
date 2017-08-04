2acf0b404f3b600403c9c5f1c76a79bda4193299
Adam Washington <adam.washington@stfc.ac.uk>
2017-08-04 14:30:07 +0100

    Load data from mantid table

--- full_analysis.py
+++ full_analysis.py
@@ -1,3 +1,4 @@
+import datetime
 from os.path import join, expanduser, exists
 import sans.command_interface.ISISCommandInterface as ici
 from mantid.api import mtd
@@ -30,45 +31,44 @@ def get_sans(run, trans, sans_can, trans_can, direct, mask, path):
     #                 "{}_1D.nxs".format(run)))
 
 SAVE_PATH="C:\\Users\\auv61894\\Documents\\Science\\Edler_May_2017\\"
-MASK = r"\\isis\inst$\NDXLARMOR\User\Users\Masks\USER_Edler_171B_a2_8mm_SEMSANS_r20287.txt"
+MASK = r"\\isis\inst$\NDXLARMOR\User"\
+       "\Users\Masks\USER_Edler_171B_a2_8mm_SEMSANS_r20287.txt"
 TUBES = [39, 40, 41]
 MASK_PATH = r'\\isis\inst$\NDXLARMOR\User\Users\Edler\May_2017\Mask_Tube{}.xml'
 
 MASKS = [MASK_PATH.format(t) for t in TUBES]
 
-def analyse():
-    with open("C:/Users/auv61894/Dropbox/Science/Edler/Edler_May_2017/rundict2.dat", "r") as infile:
-        d = json.load(infile)
-        d = {k: [RunData(*i) for i in v]
-            for k, v in d.items()}
-        if "Full Blank" not in mtd.getObjectNames():
-            int3samples(d["Full Blank"], "Full Blank", MASKS)
-        const = sel_const([mtd["Full Blank_{}".format(t)]
-                        for t, _ in enumerate(MASKS)],
-                        show_fits=True, show_quality=True)
-        for k in d.keys():
-            if "CTAB" in k:
-                continue
-            if "Full Blank" in k:
-                continue
-            if "7.81" not in k:
-                continue
-            # if "7.81" in k:
-            #     continue
-            for idx, run in enumerate(sorted(d[k])):
-                # Mantid handles sans pretty well on its own, so we'll skip it for right now
-                # get_sans(run.number, run.trans, run.csans, run.ctrans, run.direct, MASK, SAVE_PATH)
-                # sans_ws = "{}rear_1D_0.9_10.0".format(run.number)
-                # SaveAscii(sans_ws, join(SAVE_PATH,"{}_run{:02d}_sans.dat".format(k,idx)))
-                # DeleteWorkspaces([sans_ws, sans_ws+"_lab_can", sans_ws+"_lab_can_count"
-                #                 ,sans_ws+"_lab_can_norm"])
-                # DeleteWorkspace("{}_sans_nxs_monitors".format(run.number))
-                int3samples([run], "{}_run{:02d}".format(k, idx), MASKS)
-                # DeleteWorkspace("{}_sans_nxs".format(run.number))
-                semsans_ws = "{}_run{:02d}".format(k, idx)
-                norm(semsans_ws, "Full Blank", MASKS)
-                sel(semsans_ws+"_norm", const)
-                # SaveAscii(semsans_ws+"_norm_sel",
-                #           join(SAVE_PATH,"{}_run{:02d}_semsans.dat".format(k,idx)))
-                DeleteWorkspaces([semsans_ws,
-                                  semsans_ws+"_Norm"])
+def analyse(data_table):
+    runs = [
+        RunData(x["Run Number"], x["Sample"], x["Scale"], x["He3 Start"],
+                x["He3 End"], x["Trans run"], x["Can Sans run"],
+                x["Can Trans run"], x["Direct Trans run"],
+                datetime.datetime.strptime(x["Start time"],
+                                           "%Y-%m-%dT%H:%M:%S"))
+        for x in mtd[data_table]]
+
+
+    if "Full Blank" not in mtd.getObjectNames():
+        int3samples(d["Full Blank"], "Full Blank", MASKS)
+    const = sel_const([mtd["Full Blank_{}".format(t)]
+                    for t, _ in enumerate(MASKS)],
+                    show_fits=True, show_quality=True)
+
+    k = data_table[:-5]
+    for idx, run in enumerate(runs):
+        # Mantid handles sans pretty well on its own, so we'll skip it for right now
+        # get_sans(run.number, run.trans, run.csans, run.ctrans, run.direct, MASK, SAVE_PATH)
+        # sans_ws = "{}rear_1D_0.9_10.0".format(run.number)
+        # SaveAscii(sans_ws, join(SAVE_PATH,"{}_run{:02d}_sans.dat".format(k,idx)))
+        # DeleteWorkspaces([sans_ws, sans_ws+"_lab_can", sans_ws+"_lab_can_count"
+        #                 ,sans_ws+"_lab_can_norm"])
+        # DeleteWorkspace("{}_sans_nxs_monitors".format(run.number))
+        int3samples([run], "{}_run{:02d}".format(k, idx), MASKS)
+        # DeleteWorkspace("{}_sans_nxs".format(run.number))
+        semsans_ws = "{}_run{:02d}".format(k, idx)
+        norm(semsans_ws, "Full Blank", MASKS)
+        sel(semsans_ws+"_norm", const)
+        # SaveAscii(semsans_ws+"_norm_sel",
+        #           join(SAVE_PATH,"{}_run{:02d}_semsans.dat".format(k,idx)))
+        DeleteWorkspaces([semsans_ws,
+                            semsans_ws+"_Norm"])
