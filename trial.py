e5ff71ce2bea476ef4304688b813e35bdf79ab60
Adam Washington <adam.washington@stfc.ac.uk>
2017-08-10 14:21:09 +0100

    Get masks files from used

--- trial.py
+++ trial.py
@@ -1,6 +1,9 @@
 from Semsans.metadata import get_log, get_he3_log
 from Semsans.full_analysis import analyse
 
+mask_path = r'\\isis\inst$\NDXLARMOR\User\Users\Edler\May_2017\Mask_Tube{}.xml'
+masks = [mask_path.format(t) for t in [39, 40, 41]]
+
 get_he3_log(
     r"C:\Users\auv61894\Dropbox\Science\Edler\Edler_May_2017\heruns.tsv")
 get_log(range(20387,20536))
