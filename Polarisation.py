e9198ef00a08ee4b84de3a16793147f9ed550da0
Adam Washington <adam.washington@stfc.ac.uk>
2017-09-08 12:04:02 +0100

    Easily Load Polarisation

---
+++ Polarisation.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from mantid.kernel import *
+from mantid.api import *
+
+class Polarisation(PythonAlgorithm):
+    def category(self):
+        return "SESANS"
+
+    def PyInit(self):
+        self.declareProperty("RunNumber", defaultValue=0)
+        self.declareProperty(
+            WorkspaceProperty("OutputWorkspace",
+                             defaultValue="",
+                             direction=Direction.Output))
+
+    def PyExec(self):
+        w1 = Load(str(self.getProperty("RunNumber").value), LoadMonitors=True)
+        w1mon = ExtractSingleSpectrum("w1_monitors", 0)
+        w1 = ConvertUnits("w1", "Wavelength", AlignBins=1)
+        w1mon = ConvertUnits(w1mon, "Wavelength")
+        w1 = Rebin(w1, "1.8,0.1,8", PreserveEvents=False)
+        w1mon = Rebin(w1mon, "1.8,0.1,8")
+        w1 = w1 / w1mon
+        w1 = SumSpectra(w1)
+        up = mtd["w1_1"]
+        dn = mtd["w1_2"]
+        pol = (up-dn)/(up+dn)
+        self.setProperty("OutputWorkspace",pol)
+        DeleteWorkspaces([pol, "w1_1", "w1_2", "w1_monitors_1", "w1_monitors_2", "w1mon_1", "w1mon_2"])
+
+AlgorithmFactory.subscribe(Polarisation)
